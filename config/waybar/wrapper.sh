a3e3f8cf26cf493a2bfa1622e858aa874cbe1e38
alexpado <contact@alexpado.fr>
2025-05-05 02:32:03 +0200

    Base 8 !

--- config/waybar/wrapper.sh
+++ config/waybar/wrapper.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+# This wrapper allow to automatically restart waybar when a file in its config folder is modifier thus making a fake hot-reload.
 CONFIG_DIR="$HOME/.config/waybar"
 
-waybar &
+function _run {
+    if [ -f "$CONFIG_DIR/.debug" ]; then
+        GTK_DEBUG=interactive waybar &
+    else
+        waybar &
+    fi
+}
+
+_run
 
 while inotifywait -e close_write,moved_to,create -r "$CONFIG_DIR"; do
     pkill waybar
     sleep 0.3
-    waybar &
+    _run
 done
